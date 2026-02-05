be479cedd413c595068cb399c58de6fb53366373
John Mark <jcapones93@gmail.com>
2026-02-05 16:46:59 +0800

    fix the ui

--- components/animations/index.ts
+++ components/animations/index.ts
@@ -8,5 +8,3 @@
 export { ScrollReveal, type ScrollRevealProps } from './ScrollReveal';
 export { PageTransition, type PageTransitionProps } from './PageTransition';
 export { SplashCursor, type SplashCursorProps, type SplashCursorColor } from './SplashCursor';
-export { LaserFlow, type LaserFlowProps } from './LaserFlow';
-export { LaserFlowToTerminal, type LaserFlowToTerminalProps } from './LaserFlowToTerminal';
